66c0769ccd06243466648842878d69b9ed428c7c
agouram-karima <85346352+agouram-karima@users.noreply.github.com>
2021-09-11 20:58:01 +0200

    Add files via upload

---
+++ data/main.js
@@ -0,0 +1,39 @@
+
+var ecoles = {"type":"FeatureCollection", "features": [
+    { "type": "Feature", "properties": { "Comment": "ECP CHAHID OUSAADAN", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.895160012955794, 30.476607020329155, 232.220199999996112 ] } },
+{ "type": "Feature", "properties": { "Comment": "ROUDANA", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.895786012961866, 30.470210020336882, 235.44560000000638 ] } },
+{ "type": "Feature", "properties": { "Comment": "LYCEEE ZERAWI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.896476012962534, 30.469202020337548, 232.349400000006426 ] } },
+{ "type": "Feature", "properties": { "Comment": "RAHAL LMESKINI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.887320012963452, 30.473111020341243, 235.303299999999581 ] } },
+{ "type": "Feature", "properties": { "Comment": "ECOLE PRIVE HERITAGE", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.863276012971864, 30.477468020358483, 252.255099999994854 ] } },
+{ "type": "Feature", "properties": { "Comment": "ECOLE PRIVE ANAJAH", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.856039012976501, 30.476716020366407, 255.2896999999939 ] } },
+{ "type": "Feature", "properties": { "Comment": "ECOLE PRIVEE NAHJ ALFALAH", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.859024012975349, 30.476304020364026, 254.325899999996182 ] } },
+{ "type": "Feature", "properties": { "Comment": "ECOLE PRIVEE BONONI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.862276012974418, 30.475485020362001, 252.677100000000792 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC PUBLIQUE BEN SAAIDI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.86355301297519, 30.474061020362644, 253.237599999993108 ] } },
+{ "type": "Feature", "properties": { "Comment": "LYCEE PUBLIC SIDI OU SIDI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.864829012976211, 30.472339020363666, 249.91359999999986 ] } },
+{ "type": "Feature", "properties": { "Comment": "LYCEE COLEGE HASAN", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.866828012975578, 30.471922020362321, 247.997700000007171 ] } },
+{ "type": "Feature", "properties": { "Comment": "ECOLE PIVEE SABIL AL MAARIFA", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.870199012974648, 30.471077020360216, 242.689100000003236 ] } },
+{ "type": "Feature", "properties": { "Comment": "COACPSH", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.87611201297184, 30.470759020354958, 244.208499999993364 ] } },
+{ "type": "Feature", "properties": { "Comment": "GARDIER D ENFANCE LALLA AMINA", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.876138012971939, 30.470639020355129, 245.055999999996857 ] } },
+{ "type": "Feature", "properties": { "Comment": "LQ IBN SOULAYMAN ARRODANI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.876174012972234, 30.470291020355486, 243.749400000000605 ] } },
+{ "type": "Feature", "properties": { "Comment": "INSTITUT MOLAY ALHASSAN EDUCATION DES AVEUGLE", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.876276012972387, 30.470113020355633, 243.760699999998906 ] } },
+{ "type": "Feature", "properties": { "Comment": "LYCEE MOHAMED 5 EDUCATION LACIL", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.878982012969544, 30.4714930203513, 247.478900000001886 ] } },
+{ "type": "Feature", "properties": { "Comment": "EP AMINA BINTWAHAB", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.87890201296895, 30.47209902035058, 246.87390000000596 ] } },
+{ "type": "Feature", "properties": { "Comment": "GROUPE SCOLAIRE ALAMAL PRIVÉ", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.879279012968443, 30.4724150203498, 249.702699999994365 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC P IBN YASSIN", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.878130012968608, 30.47286102035028, 249.189799999992829 ] } },
+{ "type": "Feature", "properties": { "Comment": "E PRVÉ INSTITUT ITIAM INFORMATIQU", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.878147012967981, 30.473491020349442, 248.503299999996671 ] } },
+{ "type": "Feature", "properties": { "Comment": "ETABLISSEMENT MANARALIRFAN PRIVÉ", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.879181012964507, 30.47639502034464, 248.977100000003702 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC P ALWIFAQ", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.88161101296456, 30.475062020344076, 244.158899999994901 ] } },
+{ "type": "Feature", "properties": { "Comment": "FPT", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.870104012949833, 30.495743020328081, 259.09769999999844 ] } },
+{ "type": "Feature", "properties": { "Comment": "LY TECH TAROUDANT", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.87608801294445, 30.497932020319535, 261.235300000000279 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC HAY MOHAMMADI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.873868012955317, 30.48831002033425, 260.659400000004098 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC AL MAARI", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.872742012955898, 30.488333020335279, 263.664099999994505 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC TARIK2", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.877682012967822, 30.47387402034936, 249.357399999993504 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC AL AKHAWAYN", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.877757012967272, 30.474417020348621, 247.932499999995343 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC ITIAM", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.878171012968012, 30.473444020349472, 250.454899999996996 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC ABD AIIAH IBN YASSINE", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.878117012968564, 30.472943020350197, 247.411999999996624 ] } },
+{ "type": "Feature", "properties": { "Comment": "EC BAB EI KHEMIS", "DateTime": "2021-04-19" }, "geometry": { "type": "Point", "coordinates": [ -8.876926012962747, 30.479341020342982, 244.414600000003702 ] } }
+]
+}
+
+
+
